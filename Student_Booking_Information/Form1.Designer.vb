076c69b65867938eef17404883187dfe1a8b32c6
Jatinder303 <jatinder303@gmail.com>
2022-07-18 14:33:53 +1200

    Add project files.

---
+++ Student_Booking_Information/Form1.Designer.vb
@@ -0,0 +1,299 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(192, 49)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(180, 23)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(192, 158)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(129, 23)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(192, 208)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(180, 23)
+        Me.TextBox3.TabIndex = 2
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(478, 53)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(310, 379)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 15)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Student FullName"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 102)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 15)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Student DoB"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(192, 102)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 23)
+        Me.DateTimePicker1.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(27, 161)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(132, 15)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Student Phone Number"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(27, 211)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(125, 15)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Student Email Address"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(232, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(254, 25)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Student Booking Information"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(557, 33)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(135, 15)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Student Detail Summary"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(27, 272)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(44, 15)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Course"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(98, 272)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(89, 19)
+        Me.RadioButton1.TabIndex = 13
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Enginnering"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(192, 270)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(129, 19)
+        Me.RadioButton2.TabIndex = 14
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Information System"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(327, 268)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(145, 19)
+        Me.RadioButton3.TabIndex = 15
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Software Development"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(27, 327)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(158, 15)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Communication Preferences"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(206, 327)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(55, 19)
+        Me.CheckBox1.TabIndex = 17
+        Me.CheckBox1.Text = "Email"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(297, 327)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(47, 19)
+        Me.CheckBox2.TabIndex = 18
+        Me.CheckBox2.Text = "Text"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(388, 327)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(46, 19)
+        Me.CheckBox3.TabIndex = 19
+        Me.CheckBox3.Text = "Call"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.AutoEllipsis = True
+        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button1.Location = New System.Drawing.Point(192, 376)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(157, 38)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Submit Information"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.AutoEllipsis = True
+        Me.Button2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button2.ForeColor = System.Drawing.Color.Black
+        Me.Button2.Location = New System.Drawing.Point(327, 145)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(135, 45)
+        Me.Button2.TabIndex = 21
+        Me.Button2.Text = "Add More Phone Number??"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents Label8 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Student_Booking_Information/Form1.vb
@@ -0,0 +1,84 @@
+﻿Public Class Form1
+    Dim ph_number(3) As String
+    Dim counter As Integer = 0
+    Dim i As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (TextBox1.Text = "") Then 'empty validation on Student Name
+            MsgBox("Please ener the name, it can't be empty")
+        ElseIf (TextBox2.Text = "") Then
+            MsgBox("Please enter the phone number, you can't leave it empty")
+        ElseIf IsNumeric(TextBox2.Text) = False Then  'Number validation
+            MsgBox("You can only type digits for mobile number")
+
+        ElseIf (TextBox3.Text = "") Then 'Empty validation on Email
+            MsgBox("Please enter the email, it can't be empty")
+        ElseIf (RadioButton1.Checked = False And RadioButton2.Checked = False And RadioButton3.Checked = False) Then
+            MsgBox("Please select the course option")  'Validation on RadioButton
+        ElseIf (CheckBox1.Checked = False And CheckBox2.Checked = False And CheckBox3.Checked = False) Then
+            MsgBox("Please select the communication preferences") 'Validation on CheckBox
+        Else
+            ListBox1.Items.Clear() 'Clear the ListBox
+            ListBox1.Items.Add("----------------Student Detail Summary----------------------")
+            ListBox1.Items.Add("Student's Name: " + TextBox1.Text)
+            ListBox1.Items.Add("D.O.B: " + DateTimePicker1.Value.Date)
+
+            If (counter < 3) Then
+                ph_number(counter) = TextBox2.Text
+                counter = counter + 1
+            End If
+            For i = 1 To counter
+                ListBox1.Items.Add("Phone Number " + i.ToString() + " - " + ph_number(i - 1))
+            Next
+
+            ListBox1.Items.Add("Email: " + TextBox3.Text)
+                If (RadioButton1.Checked) Then
+                    ListBox1.Items.Add("Course: " + RadioButton1.Text)
+                End If
+                If (RadioButton1.Checked) Then
+                    ListBox1.Items.Add("Course: " + RadioButton2.Text)
+                End If
+                If (RadioButton3.Checked) Then
+                    ListBox1.Items.Add("Course: " + RadioButton3.Text)
+                End If
+                If (CheckBox1.Checked) Then
+                    ListBox1.Items.Add("Communication: " + CheckBox1.Text)
+                End If
+                If (CheckBox2.Checked) Then
+                    ListBox1.Items.Add("Communication: " + CheckBox2.Text)
+                End If
+                If (CheckBox3.Checked) Then
+                    ListBox1.Items.Add("Communication: " + CheckBox3.Text)
+                End If
+                ListBox1.Items.Add("-------------------Information Added-----------------------")
+            counter = 0
+            TextBox1.Clear()
+            TextBox2.Clear()
+            TextBox3.Clear()
+            RadioButton1.Checked = False
+            RadioButton2.Checked = False
+            RadioButton3.Checked = False
+            CheckBox1.Checked = False
+            CheckBox2.Checked = False
+            CheckBox3.Checked = False
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (counter < 2) Then
+            If (MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes) Then
+                If (TextBox2.Text = "") Then
+                    MsgBox("Please enter the phone number, you can't leave it empty")
+                ElseIf IsNumeric(TextBox2.Text) = False Then  'Number validation
+                    MsgBox("You can only type digits for mobile number")
+                Else
+                    ph_number(counter) = TextBox2.Text
+                    counter = counter + 1
+                    TextBox2.Text = ""
+                End If
+            End If
+        Else
+            MessageBox.Show("You can only add three phone numbers")
+        End If
+
+    End Sub
+End Class
